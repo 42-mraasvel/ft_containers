619e805ad5b531e635121af1ad6f3cc4c9cd2e50
Maarten Benjamin Raasveldt <mraasvel@student.codam.nl>
2021-09-16 15:52:36 +0200

    FINAL TEST, updated workflow

--- project/run.sh
+++ project/run.sh
@@ -1,1 +1,12 @@
-make && ./a.out
+if [ "$1" == "catch" ]; then
+	if [ "$2" == "std" ]; then
+		# ./run.sh catch std
+		make catch_std && ./catch_build_std.exe
+	else
+		# ./run.sh catch
+		make catch && ./catch_build.exe
+	fi
+else
+	# ./run.sh
+	make && ./a.out
+fi
