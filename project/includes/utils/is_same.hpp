416fa74b02885a4e55a4ab197e0dcbb7ed3eec83
Maarten Benjamin Raasveldt <mraasvel@student.codam.nl>
2021-09-17 11:46:18 +0200

    is_same now inherits from ft::true_type or ft::false_type

--- project/includes/utils/is_same.hpp
+++ project/includes/utils/is_same.hpp
@@ -8,20 +8,19 @@ namespace ft {
 	namespace _base_detail {
 	
 	template <class T, class U>
-	struct _is_same_base {
-		static const bool value = false;
-	};
+	struct _is_same_base : public ft::false_type {};
 
 	template <class T>
-	struct _is_same_base<T, T> {
-		static const bool value = true;
-	};
+	struct _is_same_base<T, T> : public ft::true_type {};
 
 	}
 
 template <class T, class U>
 struct is_same
-: public _base_detail::_is_same_base<typename ft::remove_cv<T>::type, typename ft::remove_cv<U>::type> {};
+: public _base_detail::_is_same_base<
+			typename ft::remove_cv<T>::type,
+			typename ft::remove_cv<U>::type>
+{};
 
 }
 
